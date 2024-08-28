325866c7ece0f34501989b537ab958912a405bad
jakyaafrinbristi <jakyaafrinb@gmail.com>
2024-08-28 07:38:01 +0600

    search-added

--- src/pages/ServiceCard.jsx
+++ src/pages/ServiceCard.jsx
@@ -3,44 +3,48 @@ import { GoArrowRight } from "react-icons/go";
 import { Link } from "react-router-dom";
 
 
-const ServiceCard = ({service}) => {
+const ServiceCard = ({ service }) => {
     // const {user}=useContext(AuthContext)
 
 
-    
- 
-    const {service_name,service_image,service_price,description,_id, serviceProvider}=service
 
- 
+
+    const { service_name, service_image, service_price, service_area, description, _id, serviceProvider } = service
+
+
     return (
-           <div className="max-w-2xl overflow-hidden bg-gray rounded-lg shadow-md bg-pink-50 ">
-    <img className="object-cover w-full h-64" src={service_image} alt="Article"/>
+        <div className="max-w-2xl overflow-hidden bg-gray rounded-lg shadow-md bg-pink-50 ">
+            <img className="object-cover w-full h-64" src={service_image} alt="Article" />
 
-    <div className="p-5 ">
-        <div>
-            <button className=" font-medium text-pink-500 px-2 bg-white rounded-full">Price:{service_price}</button>
-            <h3  className="block mt-2 text-xl font-semibold text-pink-500   hover:underline  ">{service_name}</h3>
-            <p className=" text-gray-900 mt-5 ">{description}</p>
-        </div>
+            <div className="p-5 ">
+                <div>
+                    <div className="flex justify-between items-center">
+                        <button className="font-bold text-pink-400 rounded-full bg-white px-2">Price:{service_price}</button>
+                        <button className="mt-2 px-2  bg-white font-bold text-pink-400 rounded-full ">{service_area}</button>
 
-        <div className="mt-4 flex items-center justify-between">
-            <div className="flex items-center">
-                <div className="flex items-center">
-                    <img className="object-cover h-10 rounded-full" src={serviceProvider?.photo} alt="Avatar"/>
-                    <p href="#" className="mx-2 font-semibold text-pink-600" >{serviceProvider ?.name}</p>
+                    </div>
+                    <h3 className="block mt-2 text-xl font-semibold text-pink-500   hover:underline  ">{service_name}</h3>
+                    <p className=" text-gray-900 mt-5 ">{description}</p>
                 </div>
-                
-                
-            </div>
-           <Link to={`/service/${_id}`} className="px-2 py-2 rounded-full bg-white"> <GoArrowRight className="text-pink-500" /></Link>
-        </div>
-   
-    </div>
-   
-</div>
 
-            
-        
+                <div className="mt-4 flex items-center justify-between">
+                    <div className="flex items-center">
+                        <div className="flex items-center">
+                            <img className="object-cover h-10 rounded-full" src={serviceProvider?.photo} alt="Avatar" />
+                            <p href="#" className="mx-2 font-semibold text-pink-600" >{serviceProvider?.name}</p>
+                        </div>
+
+
+                    </div>
+                    <Link to={`/service/${_id}`} className="px-2 py-2 rounded-full bg-white"> <GoArrowRight className="text-pink-500" /></Link>
+                </div>
+
+            </div>
+
+        </div>
+
+
+
     );
 };
 
--- src/pages/ShowAll.jsx
+++ src/pages/ShowAll.jsx
@@ -12,11 +12,12 @@ const ShowAll = () => {
     const [itemsPerPage, setItemsPerPage] = useState(2)
     const [currentPage, setCurrentPage] = useState(1)
     const [count, setCount] = useState(0)
+    const [search, setSearch] = useState('')
 
     const [services, setServices] = useState([])
     useEffect(() => {
         const getData = async () => {
-            const { data } = await axios(`${import.meta.env.VITE_API_URL}/show-all?page=${currentPage}&size=${itemsPerPage}`)
+            const { data } = await axios(`${import.meta.env.VITE_API_URL}/show-all?page=${currentPage}&size=${itemsPerPage}&search=${search}`)
             setServices(data)
 
 
@@ -24,10 +25,10 @@ const ShowAll = () => {
 
         getData()
 
-    }, [currentPage, itemsPerPage])
+    }, [currentPage, itemsPerPage,search])
     useEffect(() => {
         const getCount = async () => {
-            const { data } = await axios(`${import.meta.env.VITE_API_URL}/service-count`)
+            const { data } = await axios(`${import.meta.env.VITE_API_URL}/service-count?search=${search}`)
 
             setCount(data.count)
 
@@ -35,7 +36,7 @@ const ShowAll = () => {
 
         getCount()
 
-    }, [])
+    }, [search])
 
     console.log(count)
     const numberOfPages = Math.ceil(count / itemsPerPage)
@@ -46,6 +47,12 @@ const ShowAll = () => {
         setCurrentPage(value)
 
     }
+    const handleSearch = e => {
+        e.preventDefault()
+        const text = e.target.search.value
+        console.log(text)
+        setSearch(text)
+    }
 
     return (
         <div>
@@ -53,10 +60,11 @@ const ShowAll = () => {
                 <title>Show All</title>
             </Helmet>
             <div className="container mx-auto px-8 py-10 ">
-                <label className="input input-bordered flex items-center gap-2 mb-12 w-[50%] ml-44">
-                    <input type="text" className="grow" placeholder="Search" />
+                <form onSubmit={handleSearch}>
+                        <label className="input input-bordered flex items-center gap-2 mb-12 w-[50%] ml-44">
+                    <input type="text" name='search' className="grow" placeholder="Enter Service" />
 
-                    <svg
+                   <button> <svg
                         xmlns="http://www.w3.org/2000/svg"
                         viewBox="0 0 16 16"
                         fill="currentColor"
@@ -65,8 +73,8 @@ const ShowAll = () => {
                             fillRule="evenodd"
                             d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
                             clipRule="evenodd" />
-                    </svg>
-                </label>
+                    </svg></button>
+                </label></form>
 
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
                     {
@@ -99,10 +107,10 @@ const ShowAll = () => {
                     ))}
 
 
-                    <button 
-                    disabled = {currentPage === numberOfPages}
-                    onClick={()=>handlePaginationButton(currentPage + 1)}
-                    title="next" type="button" className="inline-flex items-center  mt-1 justify-center w-8 h-8 py-0 border rounded-md shadow-md dark:bg-gray-50 dark:border-gray-100 hover:bg-pink-500  hover:text-white">
+                    <button
+                        disabled={currentPage === numberOfPages}
+                        onClick={() => handlePaginationButton(currentPage + 1)}
+                        title="next" type="button" className="inline-flex items-center  mt-1 justify-center w-8 h-8 py-0 border rounded-md shadow-md dark:bg-gray-50 dark:border-gray-100 hover:bg-pink-500  hover:text-white">
                         <svg viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round" className="w-4">
                             <polyline points="9 18 15 12 9 6"></polyline>
                         </svg>
